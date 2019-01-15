e887e7a108fe1af7a82d16bdc833f0d8ec52d016
Didier Defrance <d2f.defrance@gmail.com>
2019-01-15 10:44:23 +0100

    TestDaoCompte.java

---
+++ src/test/java/com/m2i/tp/test/TestDaoCompte.java
@@ -0,0 +1,34 @@
+package com.m2i.tp.test;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.m2i.tp.MySpringBootApplication;
+import com.m2i.tp.dao.DaoCompte;
+import com.m2i.tp.entity.Compte;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= {MySpringBootApplication.class})
+
+public class TestDaoCompte {
+	
+	@Autowired
+	private DaoCompte daoCompte ; //à tester
+	
+	@Test
+	public void testFindByLabel() {
+		daoCompte.save(new Compte(null,"compte x",50.0));
+		daoCompte.save(new Compte(null,"compte y",60.0));
+		daoCompte.save(new Compte(null,"compte x",90.0));
+		List<Compte> listeComptes = daoCompte.findByLabel("compte x");
+		Assert.assertTrue(listeComptes.size()==2);
+		System.out.println("compte dont le label vaut compte x :" + listeComptes);
+	}
+}
