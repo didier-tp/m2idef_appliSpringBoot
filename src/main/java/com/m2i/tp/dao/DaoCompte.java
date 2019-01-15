a7f5313d8df5f7bbf88f39e89de6fb608c5529fb
Didier Defrance <d2f.defrance@gmail.com>
2019-01-15 10:14:56 +0100

    avec utilisation de spring-data sur DaoCompte

--- src/main/java/com/m2i/tp/dao/DaoCompte.java
+++ src/main/java/com/m2i/tp/dao/DaoCompte.java
@@ -1,10 +1,18 @@
 package com.m2i.tp.dao;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
 
 import com.m2i.tp.entity.Compte;
 
+//NB: avec Spring-Data , plus besoin de programmer l'implémentation jpa
+//cette classe est générée automatiquement si 
+//spring.data.jpa.repositories.enabled=true dans application.properties
+
+public interface DaoCompte extends CrudRepository<Compte, Long> {
+	//avec findById() , findAll() , save() , deleteById() , ...
+	//comme méthode prédéfinie héritées
+}
+/*
 public interface DaoCompte {
 	public Optional<Compte> findById(Long numero);
 
@@ -13,4 +21,4 @@ public interface DaoCompte {
 	public void save(Compte cpt);
 
 	public void deleteById(Long numero);
-}
+}*/
