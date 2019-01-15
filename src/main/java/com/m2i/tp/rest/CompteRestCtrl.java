507d4ea2089e6abb7a4ceec5ad17fc5aded99c8c
Didier Defrance <d2f.defrance@gmail.com>
2019-01-15 14:23:33 +0100

    WS GET ok

--- src/main/java/com/m2i/tp/rest/CompteRestCtrl.java
+++ src/main/java/com/m2i/tp/rest/CompteRestCtrl.java
@@ -2,8 +2,11 @@ package com.m2i.tp.rest;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m2i.tp.entity.Compte;
@@ -16,6 +19,15 @@ public class CompteRestCtrl {
 	@Autowired
 	private ServiceCompte serviceCompte; //service métier à injecter/utiliser
 	
+	@PostConstruct //pour compenser base réinitialisée au démarrage en mode jpa "drop-and-create"
+	public void initialiserJeuxDeDonneesEnModeDeveloppement() {
+		//code idéalement délégué à sous composant utilitaire tenant compte d'un profile spring:
+		serviceCompte.saveOrUpdateCompte(new Compte(null,"compte 1bis",100.0));
+		serviceCompte.saveOrUpdateCompte(new Compte(null,"compte 2bis",200.0));
+	}
+	
+	//URL = http://localhost:8080/appliSpringBoot/rest/compte
+	@RequestMapping(value="" , method=RequestMethod.GET)
 	public List<Compte> getComptesByCriteria(){
 		return serviceCompte.rechercherTousLesComptes();
 	}
