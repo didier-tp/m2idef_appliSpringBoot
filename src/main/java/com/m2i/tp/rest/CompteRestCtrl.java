14c4ac67a634b48ca00c06ef6a9c55bcbbf2ba97
Didier Defrance <d2f.defrance@gmail.com>
2019-01-15 15:54:25 +0100

    post

--- src/main/java/com/m2i/tp/rest/CompteRestCtrl.java
+++ src/main/java/com/m2i/tp/rest/CompteRestCtrl.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +28,18 @@ public class CompteRestCtrl {
 		serviceCompte.saveOrUpdateCompte(new Compte(null,"compte 1",100.0));
 		serviceCompte.saveOrUpdateCompte(new Compte(null,"compte 2",200.0));
 		serviceCompte.saveOrUpdateCompte(new Compte(null,"compte 3",-50.0));
+	}
+	
+	//URL = http://localhost:8080/appliSpringBoot/rest/compte en POST
+	//avec comme contenu invisible de la partie body de la requete HTTP
+	//des données json { "numero" : null , "label" : "compteXy" , "solde" : 50.0 } 
+	//           ou    { "numero" : 234 , "label" : "compteAbc" , "solde" : 70.0 }
+	@RequestMapping(value="" , method=RequestMethod.POST)
+	public Compte postCompte(@RequestBody Compte compte) {
+		serviceCompte.saveOrUpdateCompte(compte);//quelquefois auto_increment 
+		return compte; //en retour (copie des données en entrée)
+		//pour confirmer la bonne prise en compte des nouvelles valeurs
+		//et pour retourner la valeur du numero quelquefois auto incrémenté
 	}
 	
 	//URL = http://localhost:8080/appliSpringBoot/rest/compte/1
