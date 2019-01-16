4b3167cccb1f6c4a11b0e124748bca23ab8b269c
Didier Defrance <d2f.defrance@gmail.com>
2019-01-16 11:48:19 +0100

    test tva

--- src/main/java/com/m2i/tp/calcul/Calculateur.java
+++ src/main/java/com/m2i/tp/calcul/Calculateur.java
@@ -3,11 +3,12 @@ package com.m2i.tp.calcul;
 //Code avec logique stateless (proche "static")
 //testable avec @BeforeClass
 public class Calculateur {
+	
 		public double tva(double ht , double tauxTva) {
-			return 0.0;
+			return ht * tauxTva / 100;
 		}
 		
 		public double ttc(double ht , double tauxTva) {
-			return 0.0;
+			return tva(ht,tauxTva) + ht;
 		}
 }
--- src/test/java/com/m2i/tp/test/TestCalculateur.java
+++ src/test/java/com/m2i/tp/test/TestCalculateur.java
@@ -1,5 +1,39 @@
 package com.m2i.tp.test;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.m2i.tp.calcul.Calculateur;
+
 public class TestCalculateur {
+	
+	private static Calculateur calculateur; //composant à tester
+	
+	public TestCalculateur() {
+		System.out.println("instance de TestCalculateur construite:" + this);
+		//NB si méthode toString() pas recodée sur Calculateur et TestCalculateur
+		//la version par défaut affiche type et référence mémoire de l'instance
+	}
+	
+	@BeforeClass
+	public static void initCalculateur() {
+		calculateur = new Calculateur();
+		System.out.println("instance de Calculateur inialisée:" + calculateur);
+	}
+	
+	@Test
+	public void testTva() {
+		double tvaCalculee = calculateur.tva(200.0,20.0);
+		System.out.println("tva calculee:" + tvaCalculee + " via instance " + calculateur);
+		Assert.assertEquals(40.0,tvaCalculee,0.000001);
+	}
+	
+	@Test
+    public void testTtc() {
+    	double ttcCalcule = calculateur.ttc(200.0,20.0);
+    	System.out.println("ttc calcule:" + ttcCalcule + " via instance " + calculateur);
+		Assert.assertEquals(240.0,ttcCalcule,0.000001);
+	}
 
 }
